d323823d6a3c7ca994f8c7e6d508ba795e6c0463
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-11-16 20:07:38 +0530

    ecommerce task-8

--- src/App.js
+++ src/App.js
@@ -1,38 +1,89 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
 import MoviesList from './components/MoviesList'
 import './App.css'
 
 function App() {
-  const [movies, setMovies] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [Movies, setMovies] = useState([])
+  const [isLoading, setisLoading] = useState(false)
+  const [error, seterror] = useState(null)
 
-  async function fetchMoviesHandler() {
-    setIsLoading(true)
-    const resp = await fetch('https://swapi.dev/api/films')
+  const fetchdataHandler = async () => {
+    setisLoading(true)
+    seterror(null)
+    try {
+      const responce = await fetch('https://swapi.dev/api/fi2lms')
 
-    const data = await resp.json()
-
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
+      if (!responce.ok) {
+        throw new Error('somting went wrong')
       }
-    })
-    setMovies(transformedMovies)
-    setIsLoading(false)
+
+      const data = await responce.json()
+
+      const Listmovie = data.results.map((item) => {
+        return {
+          id: item.episode_id,
+          title: item.title,
+          releaseDate: item.release_date,
+          openingText: item.opening_crawl,
+        }
+      })
+      setMovies([...Listmovie])
+      setisLoading(false)
+      //console.log(data)
+
+      console.log('am fetch data')
+    } catch (err) {
+      seterror(err.message)
+
+      //console.log(err.message)
+    }
+    setisLoading(false)
+  }
+
+  useEffect(() => {
+    if (error) {
+      seterror('somting went wrong .....Retrying')
+      let intervelid = setInterval(() => {
+        fetchdataHandler()
+        console.log('calling ', intervelid)
+      }, 5000)
+
+      return () => {
+        console.log('cancel', intervelid)
+        clearInterval(intervelid)
+      }
+    }
+  }, [error])
+
+  //  }
+  const cancelIntervel = () => {
+    seterror(null)
+    //console.log('am cancel',intervelid)
+    // clearInterval(intervelid)
+  }
+
+  let Content = <p>No Movies In The List</p>
+  if (Movies.length > 0) {
+    Content = <MoviesList movies={Movies} />
+  }
+  if (error) {
+    Content = <p>{error}</p>
+  }
+  if (isLoading == true) {
+    Content = <p>Loading.....</p>
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={fetchdataHandler}>Fetch Movies</button>
+        <button onClick={cancelIntervel}>Cancel Request</button>
       </section>
       <section>
-        {!isLoading && <MoviesList movies={movies} />}
-        {isLoading && <p>Loading...</p>}
+        {Content}
+        {/* {!isLoading&& Movies.length>0&& <MoviesList movies={Movies} />}
+        {!isLoading&& Movies.length===0&& <p>No movies Found</p>}
+        {isLoading&&<p>Loading.....</p>} */}
       </section>
     </React.Fragment>
   )
